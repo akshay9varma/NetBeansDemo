3e012eda1cd5e4e8f67ad59a6bf7a93a99302138
akshay9varma <52049904+akshay9varma@users.noreply.github.com>
2019-07-11 14:44:00 -0400

    Update JavaApplication1.java

--- Documents/NetBeansProjects/JavaApplication1/src/javaapplication1/JavaApplication1.java
+++ Documents/NetBeansProjects/JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -7,6 +7,7 @@ public class JavaApplication1 {
     public static void main(String[] args) {
         System.out.println("Welcome");
         System.out.println("Line 2");
+        System.ou.println("Good Byee")
     }
     
 }
